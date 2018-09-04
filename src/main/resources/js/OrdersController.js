3a46945bc6e3fc9042bb8435ecff8bde03dbde62
danielagon <daniela.gonzalezramirez@hotmail.com>
2018-09-03 21:12:44 -0500

    Part 1

---
+++ src/main/resources/js/OrdersController.js
@@ -0,0 +1,26 @@
+var order2 = {
+	"order_id": 2,
+	"table_id": 2,
+	"products": [{
+                    "product": "PIZZA",
+                    "quantity": 3,
+                    "price": "$10.000"
+		},
+		{
+                    "product": "HOTDOG",
+                    "quantity": 1,
+                    "price": "$3.000"
+		},
+		{
+                    "product":"COKE",
+                    "quantity":4,
+                    "price":"$1.300"
+		}
+	]
+};
+
+function addOrder(order){
+	var table = document.createElement("Table");
+}
+
+
